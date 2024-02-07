ab66092c6235133511c8b7c9dd61e05840705dfd
Guilherme <guilherme.sajs@gmail.com>
2024-02-07 09:05:51 -0300

    DESVIO COMPOSTO O comando anterior (SE ENTÃO) executava somente o bloco verdade, o lado verdadeiro da condição, ou seja, não existia o bloco falso. O desvio composto tem tanto o <bloco verdadeiro> quanto o <bloco falso>. O comando SE ENTÃO SENÃO é utilizado quando o desvio for composto por dois blocos, o verdadeiro e o falso. Este comando é uma bifurcação. Um de dois caminhos é seguido a partir da resposta de uma condição pré-estabelecida (depois da palavra SE), caso a condição resulte verdade, será executado o <bloco verdade> (que é representado pelo ENTÃO), caso contrário será executado o <bloco falso> (que é representado pelo SENÃO). Independentemente do bloco executado, o algoritmo segue a sequência do fluxo.

---
+++ src/exemplo_2.java
@@ -0,0 +1,29 @@
+//DESVIO COMPOSTO
+//Uma empresa dará aumento para os seus funcionários.
+//Caso ele trabalhe menos de três anos na empresa, ganhará 5% de aumento, caso contrário 10%.
+
+import java.util.Scanner;
+
+public class exemplo_2 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        int tempo_empresa;
+        double sal, aumento, novo_sal;
+
+        System.out.println("Digite o seu tempo de casa: ");
+        tempo_empresa = entrada.nextInt();
+        System.out.println("Digite o seu salário: ");
+        sal = entrada.nextDouble();
+
+        if (tempo_empresa < 3) {
+            aumento = sal * 0.05;
+        }
+        else {
+            aumento = sal * 0.1;
+        }
+            novo_sal = sal + aumento;
+            System.out.println("O seu salário foi de " + sal + " para " + novo_sal);
+    }
+}
+
