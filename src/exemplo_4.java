ce76b02ecf5b314e6e48d6d9a7b8d437e87e804e
Guilherme <guilherme.sajs@gmail.com>
2024-02-08 03:36:29 -0300

    Switch e Break Escolhendo o dia da semana ultilizando o switch e o break

---
+++ src/exemplo_4.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+//Dado um número pelo usuário, exibir o dia da semana correspondente.
+public class exemplo_4 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int dia;
+
+        System.out.println("Digite um número: ");
+        dia = entrada.nextInt();
+
+        switch(dia)
+        {
+            case 1: System.out.println("Domingo"); break;
+            case 2: System.out.println("Segunda-feira"); break;
+            case 3: System.out.println("Terça-feira"); break;
+            case 4: System.out.println("Quarta-feira"); break;
+            case 5: System.out.println("Quinta-feira"); break;
+            case 6: System.out.println("Sexta-feira"); break;
+            case 7: System.out.println("Sábado"); break;
+            default: System.out.println("Dia inválido"); break;
+        }
+    }
+}
