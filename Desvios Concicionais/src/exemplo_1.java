b3f4bc5385d1d50d945c4cb43beba398fd6e7d89
Guilherme <guilherme.sajs@gmail.com>
2024-02-10 02:53:46 -0300

    Ajustando os códigos

--- Desvios Concicionais/src/exemplo_1.java
+++ Desvios Concicionais/src/exemplo_1.java
@@ -2,10 +2,11 @@ import java.util.Scanner;
 // DESVIO SIMPLES
 // Exemplo 1
 // Dada uma venda, calcular e exibir o desconto de 10% caso a venda seja acima de 300 reais.
-public class exemplos_saida {
+public class exemplo_1 {
    public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         double venda, desconto;
+        
         System.out.println("Digite o valor de venda: ");
         venda = entrada.nextDouble();
         if (venda > 300) {
