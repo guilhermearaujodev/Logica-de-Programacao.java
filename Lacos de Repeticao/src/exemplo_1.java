60e148861a4829f0932c650360ae6c5be6fa4bd2
Guilherme <guilherme.sajs@gmail.com>
2024-02-10 05:10:07 -0300

    Estruturas de repetição - Laço Pré-condicional: Enquanto-faça Conhecidos como Looping  e  Laços.  Este  apelido  se  dá  pelo  fato  dele iniciar  e  terminar  no  mesmo ponto e a sua “volta” envolver o bloco de repetição. O Laço Pré-condicional é aquele que primeiro  é  analisada  uma  condição  e  depois  executado  o Bloco de  comandos envolvidos pelo laço,ou seja, que serão repetidos. O laço Enquanto-Faça tem a característica de ser o laço “0,N”, no qual pode ocorrer de não executar nenhuma volta (zero) ou várias(N). Este é o laço que tem a característica de primeiro analisar e depois executar.

---
+++ Lacos de Repeticao/src/exemplo_1.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+//Laço Pré-condicional
+//Solicitar ao usuário que digite S para sim ou N para não obrigatoriamente.
+//Se digitar uma letra que não for S ou N, realizar a repetição.
+//while = enquanto
+//! = negação (depende da maneira que for usada)
+public class exemplo_1 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        String opcao;
+
+        System.out.println("Digite [S]im ou [N]ão: ");
+        opcao = entrada.next();
+
+        while(!(opcao.equals("s") || opcao.equals("S") || opcao.equals("n") || opcao.equals("N")))
+        {
+            System.out.println("Você digitou " + opcao + " digite S ou N!");
+
+            System.out.println("Digite [S]im ou [N]ão: ");
+            opcao = entrada.next();
+        }
+        System.out.println("Você digitou " + opcao);
+
+    }
+}
