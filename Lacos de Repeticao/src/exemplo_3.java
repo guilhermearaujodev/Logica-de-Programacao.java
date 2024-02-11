170beb42052e579de2adddeea11de3d0a63cb34a
Guilherme <guilherme.sajs@gmail.com>
2024-02-11 04:38:16 -0300

    LAÇO FAÇA-ENQUANTO É um Laço Pós-condicional–inverso do Enquanto-Faça Primeiro é executado o Bloco de Repetição e depois é analisada a condição. A característica  de  ser  o  laço  “1,N”. Com esta característica,o laço executa o bloco de repetição ao menos uma(1) vez ou várias(N).

---
+++ Lacos de Repeticao/src/exemplo_3.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+//Laço Faça-enquanto
+//Somar números digitados pelo usuário até que ele digite um número negativo.
+//(O número negativo não deve fazer parte da soma)
+public class exemplo_3 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        int num, soma = 0;
+
+        do{
+            System.out.println("Digite um número: ");
+            num = entrada.nextInt();
+            if (num > 0)
+            {
+                soma = soma + num;
+            }
+        }while (num >= 0);
+
+        System.out.println("Soma = " + soma);
+    }
+}
