618e08cbd078ae74e8e982caf57427f1409268be
Guilherme <guilherme.sajs@gmail.com>
2024-02-12 02:52:36 -0300

    Procedimentos sem Parâmetros Os  subalgoritmos  são  representados  por funções e  procedimentos, com passagem de parâmetros ou não. Quando decidimos criar um subalgoritmo, consideramos, entre outras coisas, que  ele  será  executado  em  outros momentos  da  aplicação  ou  em  até  outras aplicações. Procedimento é  um  tipo  de  subalgoritmo  que  executa  as  instruções  que compõem um objetivo e que não retorna valor ao programa chamador.

---
+++ Funcoes, Procedimentos, Vetores e Matrizes/src/exemplo_1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class exemplo_1 {
+    //criação do procedimento
+    public static void saudacao() {
+        System.out.println("Seja bem-vindo");
+    }
+    public static void main(String[] args) {
+        saudacao();
+    }
+}
