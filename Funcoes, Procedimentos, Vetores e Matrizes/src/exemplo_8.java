7eea03063a96fe05757430349934b88f3a2b5fde
Guilherme <guilherme.sajs@gmail.com>
2024-02-15 03:06:07 -0300

    Vetores Variáveis indexadas são aquelas em que podemos guardar várias informações e a sua manipulação é feita pelo índice, que fica entre colchetes. O índice sempre inicia do 0 (zero) e vai até o limite do vetor. Temos a variável indexada unidimensional(uma linha e diversas colunas), que é o vetor, e a bidimensional (diversas linhas e diversas colunas), que é a matriz. As variáveis indexadas são os conhecidos vetores e matrizes; elas também são conhecidas como listas ou até tuplas (vetor de constantes). Como variáveis de memória, um vetor deve ter um nome e um tipo. Como variáveis de memória, um vetor deve ter um nome e um tipo. A mudança fica na definição do tamanho do vetor, ou seja, a quantidade de elementos que ele terá.

---
+++ Funcoes, Procedimentos, Vetores e Matrizes/src/exemplo_8.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class exemplo_8 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        int vetor[] = new int[5];
+        int i;
+
+        //Preenchendo o vetor
+        System.out.println("Preenchendo o vetor...");
+        for(i = 0; i < 5; i++) // i = 0
+        {
+            System.out.println("Posição [" + i + "] = ");
+            vetor[i] = entrada.nextInt();
+        }
+
+        //Exibindo o vetor
+        System.out.println("\nExibindo o vetor...");
+        for (i = 0; i < 5; i++)
+        {
+            System.out.println("Vetor[" + i + "] = " + vetor[i]);
+        }
+
+        //Somando o vetor
+        int soma = 0;
+        for (i = 0; i < 5; i++)
+        {
+            soma = soma + vetor[i];
+        }
+        System.out.println("\nSoma dos elemtnso = " + soma);
+    }
+}
